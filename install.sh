483262ba615aed0082f7f004138618d7da1b53b8
tanduong <tanduong@live.com>
2020-02-13 07:26:53 -0600

    PLAT 1223

--- install.sh
+++ install.sh
@@ -16,14 +16,24 @@ source blueprint-lib/init.sh
 # APP_NAME is the name of the Django app that will be modified
 ##
 
-BLUEPRINT="ArticlesBlueprint"
-NAME="Articles"
+BLUEPRINT="$3"
+NAME="$4"
+
+if [ -z "$BLUEPRINT" ]
+then
+    BLUEPRINT="ArticlesBlueprint"
+fi
+
+if [ -z "$NAME" ] 
+then
+    NAME="Articles"
+fi
 
 EXT_POINT_1="@BlueprintInsertion"
 EXT_POINT_2="@BlueprintImportInsertion"
 EXT_POINT_3="@BlueprintNavigationInsertion"
 DATA_1="{ name: '${BLUEPRINT}', human_name: '${NAME}', access_route: '${BLUEPRINT}'},"
-DATA_2="import { ${BLUEPRINT}Navigator } from '..\/features\/${BLUEPRINT}\/navigator';"
+DATA_2="import ${BLUEPRINT}Navigator from '..\/features\/${BLUEPRINT}\/navigator';"
 DATA_3="${BLUEPRINT}: { screen: ${BLUEPRINT}Navigator },"
 
 echo "create blueprint folder"
